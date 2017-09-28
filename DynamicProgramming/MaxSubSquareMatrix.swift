94cd445c252b9bc6cb6bfb210f06daf305f51b94
Vinodh Swamy <vinodh.g.in@gmail.com>
2017-09-28 19:59:40 +0530

    Added MaxSubSquareMatrix class as a separate file so easily visible in git repo

---
+++ DynamicProgramming/MaxSubSquareMatrix.swift
@@ -0,0 +1,50 @@
+//
+//  Trie.swift
+//  Trie-Datastructure-Example
+//
+//  Created by Vinodh Govind Swamy on 8/5/17.
+//  Copyright © 2017 Vinodh Swamy. All rights reserved.
+//
+
+import Foundation
+
+class MaxSubSqaureMatrix {
+    private func minOf(num1:Int, num2:Int, num3:Int) -> Int {
+        let rowMin = min(num1, num2)
+        return min(rowMin, num3)
+    }
+    
+    
+    func maxSubSqaureMatrix(matrix:[[Int]]) -> ([Int], [Int]) {
+        let rowCount = matrix.count
+        let colCount = matrix[0].count
+        var endRowIndex = 0
+        var endColIndex = 0
+        var max = 0
+        
+        var result = matrix
+        print("Matrix")
+        print(result)
+        
+        
+        for row in 1..<rowCount {
+            for col in 1..<colCount {
+                if result[row][col] == 0 {
+                    continue
+                }
+                
+                let value = minOf(num1: result[row - 1][col], num2: result[row - 1][col - 1], num3: result[row][col-1])
+                result[row][col] = value + 1
+                
+                if result[row][col] > max {
+                    max = result[row][col]
+                    endRowIndex = row
+                    endColIndex = col
+                }
+            }
+        }
+        print("Result")
+        print(result)
+        return ([endRowIndex - max + 1, endColIndex - max + 1], [endRowIndex, endColIndex])
+    }
+}
