0e47ae2620345673103fe657607543a88d9bd1ae
Vinodh Swamy <Vinodh.Govindaswamy@walmartlabs.com>
2020-10-03 19:17:52 +0530

    add one to digits

---
+++ Array/Add-One-To-Number.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+func plusOne(numDigits: [Int]) -> [Int] {
+    guard !numDigits.isEmpty else { return numDigits }
+    
+    var result: [Int] = []
+    let digitToAdd = 1, base = 10
+    var index = numDigits.count - 1
+    
+    var carry = 0
+    let newDigit = numDigits[index] + digitToAdd
+    result.append(newDigit % base)
+    carry = newDigit / base
+    index -= 1
+    
+    while index >= 0 {
+        
+        let newDigit = numDigits[index] + carry
+        result.append(newDigit % base)
+        carry = newDigit / base
+        index -= 1
+    }
+    
+    return result.reversed()
+}
+
+print(plusOne(numDigits: [1, 2, 3]))
+
+print(plusOne(numDigits: [0, 7, 9, 9]))
+
+print(plusOne(numDigits: [0, 2, 9, 9, 9, 9, 9, 9]))
